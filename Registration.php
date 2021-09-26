c6235f3bc5a7a022d55a9a11156e5cf6f8209d0a
Turzo Roy <84925906+Turzo2134@users.noreply.github.com>
2021-09-26 13:15:35 +0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,99 @@
+<html>
+<body>
+<form>
+	<table>
+    <tr>
+			<td>
+				First Name:
+			</td>
+
+			<td>
+				<input type=”name”>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Last Name:
+			</td>
+
+			<td>
+				<input type=”name”>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Age:
+			</td>
+
+			<td>
+				<input type=”age”>
+			</td>
+		</tr>
+		<tr>
+        <td>
+				Designation:
+			</td>
+            <td>
+        <input type="checkbox" name="Jounior Programmer" value="Jounior Programmer">
+		Jounior Programmer
+</td>
+<td>
+         <input type="checkbox" name="Senior Programmer" value="Senior Programmer">
+		 Senior Programmer
+</td>
+<td>
+         <input type="checkbox" name="Project lead" value="Project lead">
+          Project lead
+</td>
+       
+</tr>
+<tr>
+        <td>
+				Preferred language:
+			</td>
+            <td>
+        <input type="checkbox" name="JAVA" value="JAVA">
+        JAVA
+</td>
+<td>
+         <input type="checkbox" name="PHP" value="PHP">
+         PHP
+</td>
+<td>
+         <input type="checkbox" name="C++" value="C++">
+         C++
+</td>
+       
+</tr>
+		<tr>
+			<td>
+				E-mail:
+			</td>
+
+			<td>
+				<input type=”mail”>
+			</td>
+		</tr>
+<tr>
+<td>
+				Password:
+			</td>
+
+			<td>
+				<input type=”Password”>
+				
+			</td>
+		</tr>
+        
+       
+<td>
+<label for="myfile">
+    Select a file:</label>
+<input type="file" id="myfile" name="myfile">
+</td>
+	</table>
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+</form> 
+</body>
+</html>
